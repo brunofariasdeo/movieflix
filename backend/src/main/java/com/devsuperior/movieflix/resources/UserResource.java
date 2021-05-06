0e8647fb8fca2d7003cef5222fb0a9bd84472484
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-05-06 02:31:38 -0300

    Create UserDTO and set it on resource and service

--- backend/src/main/java/com/devsuperior/movieflix/resources/UserResource.java
+++ backend/src/main/java/com/devsuperior/movieflix/resources/UserResource.java
@@ -1,6 +1,5 @@
 package com.devsuperior.movieflix.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.services.UserService;
 
@@ -20,8 +20,8 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){		
-		List<User> list = service.findAll();
+	public ResponseEntity<List<UserDTO>> findAll(){		
+		List<UserDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
--- backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.movieflix.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.UserRepository;
 
@@ -14,7 +17,16 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
-	public List<User> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly=true)
+	public List<UserDTO> findAll(){
+		List<User> list = repository.findAll();
+		
+		List<UserDTO> listDTO = new ArrayList<>();
+		
+		for(User user : list) {
+			listDTO.add(new UserDTO(user));
+		}
+		
+		return listDTO;
 	}
 }
