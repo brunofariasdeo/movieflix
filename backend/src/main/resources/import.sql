25431539177974ccb55d75c2eb24ca2a269425ac
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-05-06 02:32:11 -0300

    Create sql file to populate H2 database

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_user (name,email,password) VALUES ('Bruno','bruno@gmail.com','123456');
+INSERT INTO tb_user (name,email,password) VALUES ('Bianca','bianca@gmail.com','123456');
+INSERT INTO tb_user (name,email,password) VALUES ('Danilo','danilo@gmail.com','123456'); 
