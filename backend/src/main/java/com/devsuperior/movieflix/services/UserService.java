586c4f229961e6c9c5e58913908cf21afef97958
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-05-07 01:11:41 -0300

    Create PUT route to User entity and rename exception class

--- backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.entities.User;
-import com.devsuperior.movieflix.exceptions.EntityNotFoundException;
+import com.devsuperior.movieflix.exceptions.ResourceNotFoundException;
 import com.devsuperior.movieflix.repositories.UserRepository;
 
 @Service
@@ -35,7 +37,7 @@ public class UserService {
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		User entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		
 		return new UserDTO(entity);
 	}
@@ -49,5 +51,20 @@ public class UserService {
 		entity = repository.save(entity);
 		
 		return new UserDTO(entity);
+	}
+
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = repository.getOne(id);
+			entity.setEmail(dto.getEmail());
+			entity.setName(dto.getName());
+			entity.setPassword(dto.getPassword());
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		}
+		catch(EntityNotFoundException e){
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 }
