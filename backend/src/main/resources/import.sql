aa98bdf93d7914696468e247240c0e1bd7769903
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-05-08 19:32:42 -0300

    Populate sql file with movies

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,7 @@
 INSERT INTO tb_user (name,email,password, created_At) VALUES ('Bruno','bruno@gmail.com','123456', NOW());
 INSERT INTO tb_user (name,email,password, created_At) VALUES ('Bianca','bianca@gmail.com','123456', NOW());
 INSERT INTO tb_user (name,email,password, created_At) VALUES ('Danilo','danilo@gmail.com','123456', NOW()); 
+
+INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, created_At) VALUES ('Fuga das Galinhas','Subtitle', 1994,'Image URL', 'Movie synopsis', NOW()); 
+INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, created_At) VALUES ('The Godfather','Subtitle', 1988,'Image URL', 'Movie synopsis', NOW()); 
+INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, created_At) VALUES ('Fast and Furious','Subtitle', 2001,'Image URL', 'Movie synopsis', NOW()); 
