452dcfd5d451133935ae83bcdfbfb1b8386b6d31
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-05-08 22:38:07 -0300

    Populate sql file with genres

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -5,3 +5,7 @@ INSERT INTO tb_user (name,email,password, created_At) VALUES ('Danilo','danilo@g
 INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, created_At) VALUES ('Fuga das Galinhas','Subtitle', 1994,'Image URL', 'Movie synopsis', NOW()); 
 INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, created_At) VALUES ('The Godfather','Subtitle', 1988,'Image URL', 'Movie synopsis', NOW()); 
 INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, created_At) VALUES ('Fast and Furious','Subtitle', 2001,'Image URL', 'Movie synopsis', NOW()); 
+
+INSERT INTO tb_genre (name) VALUES ('Ação');
+INSERT INTO tb_genre (name) VALUES ('Aventura');
+INSERT INTO tb_genre (name) VALUES ('Comédia');
