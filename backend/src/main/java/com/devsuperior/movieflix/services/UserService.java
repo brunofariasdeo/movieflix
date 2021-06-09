07bbfb77c3d38715e7add8bf0e14fabc96f30884
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-06-09 00:37:23 -0300

    Add OAuth security

--- backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,13 +27,15 @@ import com.devsuperior.movieflix.repositories.RoleRepository;
 import com.devsuperior.movieflix.repositories.UserRepository;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserRepository repository;
 	
 	@Autowired
 	private RoleRepository roleRepository;
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Transactional(readOnly = true)
 	public Page<UserDTO> findAllPaged(PageRequest pageRequest){
@@ -91,5 +98,19 @@ public class UserService {
 			Role role = roleRepository.getOne(roleDTO.getId());
 			entity.getRoles().add(role);
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User user = repository.findByEmail(username);
+
+		if (user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+
+		logger.info("User found: " + username);
+		return user;
 	}
 }
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,21 +1,23 @@
-INSERT INTO tb_user (name,email,password) VALUES ('Bruno','bruno@gmail.com','123456');
-INSERT INTO tb_user (name,email,password) VALUES ('Bianca','bianca@gmail.com','123456');
-INSERT INTO tb_user (name,email,password) VALUES ('Danilo','danilo@gmail.com','123456'); 
+INSERT INTO tb_user (name,email,password) VALUES ('Bob','bob@gmail.com','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name,email,password) VALUES ('Ana','ana@gmail.com','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name,email,password) VALUES ('Danilo','danilo@gmail.com','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG'); 
 
 INSERT INTO tb_role(authority) VALUES ('MEMBER');
 INSERT INTO tb_role(authority) VALUES ('VISITOR');
 
-INSERT INTO tb_user_role (user_id, role_id) VALUES (1,1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1,2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2,1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3,2);
 
 INSERT INTO tb_genre (name) VALUES ('Ação');
 INSERT INTO tb_genre (name) VALUES ('Aventura');
 INSERT INTO tb_genre (name) VALUES ('Comédia');
+INSERT INTO tb_genre (name) VALUES ('Drama');
 
-INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, genre_id) VALUES ('Fuga das Galinhas','Subtitle', 1994,'Image URL', 'Movie synopsis', 1); 
-INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, genre_id) VALUES ('The Godfather','Subtitle', 1988,'Image URL', 'Movie synopsis', 2); 
-INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, genre_id) VALUES ('Fast and Furious','Subtitle', 2001,'Image URL', 'Movie synopsis', 3); 
+INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, genre_id) VALUES ('Fuga das Galinhas','Subtitle', 1994,'Image URL', 'Movie synopsis', 3); 
+INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, genre_id) VALUES ('Minha mãe é uma peça','Subtitle', 2018,'Image URL', 'Movie synopsis', 1); 
+INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, genre_id) VALUES ('The Godfather','Subtitle', 1988,'Image URL', 'Movie synopsis', 4); 
+INSERT INTO tb_movie (title, subtitle, year, img_url, synopsis, genre_id) VALUES ('Fast and Furious','Subtitle', 2001,'Image URL', 'Movie synopsis', 1); 
 
 INSERT INTO tb_review (text, user_id, movie_id) VALUES ('Muito bom!', 1, 1);
 INSERT INTO tb_review (text, user_id, movie_id) VALUES ('Não gostei, achei fraco.', 2, 3);
